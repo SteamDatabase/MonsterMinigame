2fa8df7af3b5b7a54061a52934de8cd5712c3e53
Contex <contexmoh@gmail.com>
2015-06-18 23:04:32 +0200

    Add GetGameData UDP

--- api/ITowerAttackMiniGameService/GetGameData/v0001/index.php
+++ api/ITowerAttackMiniGameService/GetGameData/v0001/index.php
@@ -5,58 +5,20 @@
 		die;
 	}
 	
+	if( empty( $_GET[ 'gameid' ] ) )
+	{
+		http_response_code( 400 );
+		die;
+	}
+
+	// TODO: include_stats=1 query
+	
 	header( 'Content-type: application/json' );
 	
-	require __DIR__ . '/../../Enums.php';
+	$Data = json_encode(array(
+		'request' => 'GetGameData',
+		'gameid' => $_GET[ 'gameid' ]
+	));
 	
-	$Lane =
-	[
-		'element' => 4,
-		'dps' => 0,
-		'active_player_ability_gold_per_click' => 0,
-		'active_player_ability_decrease_cooldowns' => 0.25,
-		'active_player_abilities' =>
-		[
-			
-		],
-		'player_hp_buckets' =>
-		[
-			
-		],
-		'enemies' =>
-		[
-			[
-				'id' => 2353793,
-				'type' => ETowerAttackEnemyType::Boss,
-				'hp' => 6851814772839,
-				'max_hp' => 6851814772839,
-				'dps' => 2805538,
-				'timer' => 1.01,
-				'gold' => 118943606
-			]
-		]
-	];
-	
-	$GameData =
-	[
-		'level' => 1000000 - 1, // level is zero based
-		'status' => EMiniGameStatus::Running,
-		'timestamp' => time(),
-		'timestamp_game_start' => 1337,
-		'timestamp_level_start' => 1338,
-		'lanes' =>
-		[
-			$Lane,
-			$Lane,
-			$Lane
-		]
-	];
-	
-	echo json_encode(
-		[
-			'response' =>
-			[
-				'game_data' => $GameData
-			]
-		] 
-	);
+	$Socket = socket_create( AF_INET, SOCK_DGRAM, SOL_UDP ); 
+	socket_sendto( $Socket, $Data, strlen( $Data ), 0, '127.0.0.1', 5337 );
