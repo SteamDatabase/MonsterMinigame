62a87fcd0a8deca461248cdaba7595f04287a78c
Contex <contexmoh@gmail.com>
2015-06-19 02:05:31 +0200

    Added more methods for player class

--- server.php
+++ server.php
@@ -78,7 +78,7 @@ class CTowerAttackServer
 						if( $Player !== null ) {
 							$Response = array(
 								'player_data' => $Player->ToArray(),
-								'tech_tree' => array() //TODO
+								'tech_tree' => $Player->GetTechTree()->ToArray()
 							);
 						}
 					}
@@ -379,7 +379,8 @@ class CTowerAttackPlayer
 		$ActiveAbilitiesBitfield,
 		$ActiveAbilities,
 		$CritDamage,
-		$Loot
+		$Loot,
+		CTowerAttackPlayerTechTree $TechTree
 	)
 	{
 		$this->AccountId = $AccountId;
@@ -392,6 +393,7 @@ class CTowerAttackPlayer
 		$this->ActiveAbilities = $ActiveAbilities;
 		$this->CritDamage = $CritDamage;
 		$this->Loot = $Loot;
+		$this->TechTree = $TechTree;
 	}
 
 	public function ToArray()
@@ -405,6 +407,11 @@ class CTowerAttackPlayer
 			'active_abilities_bitfield' => $this->GetActiveAbilitiesBitfield(),
 			'crit_damage' => $this->GetCritDamage()
 		);
+	}
+
+	public function GetTechTree()
+	{
+		return $this->TechTree;
 	}
 
 	public function GetAccountId()
@@ -455,6 +462,262 @@ class CTowerAttackPlayer
 	public function GetLoot()
 	{
 		return $this->Loot;
+	}
+}
+
+class CTowerAttackPlayerTechTree
+{
+	/*
+	repeated Upgrade upgrades = 1;		
+	optional double damage_per_click = 2 [default = 1.0];
+	optional double damage_multiplier_fire = 3 [default = 1.0];
+	optional double damage_multiplier_water = 4 [default = 1.0];
+	optional double damage_multiplier_air = 5 [default = 1.0];
+	optional double damage_multiplier_earth = 6 [default = 1.0];
+	optional double damage_multiplier_crit = 7 [default = 2.0];
+	optional uint64 unlocked_abilities_bitfield = 8 [default = 0];
+	optional double hp_multiplier = 9 [default = 1.0];
+	optional double crit_percentage = 10 [default = 0];
+	optional double badge_points = 11;
+	repeated AbilityItem ability_items = 12;
+	optional double boss_loot_drop_percentage = 13 [default = 0.25];
+	optional double damage_multiplier_dps = 14 [default = 1.0];
+	optional double base_dps = 15;
+	optional double damage_per_click_multiplier = 16 [default = 1.0];
+	optional double max_hp = 17;
+	optional double dps = 18;
+	*/
+	private $Upgrades = array();
+	private $DamagePerClick = 1.0;
+	private $DamageMultiplierFire = 1.0;
+	private $DamageMultiplierWater = 1.0;
+	private $DamageMultiplierAir = 1.0;
+	private $DamageMultiplierEarth = 1.0;
+	private $DamageMultiplierCrit = 2.0;
+	private $UnlockedAbilitiesBitfield = 0;
+	private $HpMultiplier = 1.0;
+	private $CritPercentage = 0;
+	private $BadgePoints = 0;
+	private $AbilityItems = array();
+	private $BossLootDropPercentage = 0.25;
+	private $DamageMultiplierDps = 1.0;
+	private $BaseDps = 0;
+	private $DamagePerClickMultiplier = 1.0;
+	private $MaxHp = 0;
+	private $Dps = 0;
+	
+	public function __construct( 
+		$Upgrades = array(),
+		$DamagePerClick = 1.0,
+		$DamageMultiplierFire = 1.0,
+		$DamageMultiplierWater = 1.0,
+		$DamageMultiplierAir = 1.0,
+		$DamageMultiplierEarth = 1.0,
+		$DamageMultiplierCrit = 2.0,
+		$UnlockedAbilitiesBitfield = 0,
+		$HpMultiplier = 1.0,
+		$CritPercentage = 0,
+		$BadgePoints = 0,
+		$AbilityItems = array(),
+		$BossLootDropPercentage = 0.25,
+		$DamageMultiplierDps = 1.0,
+		$BaseDps = 0,
+		$DamagePerClickMultiplier = 1.0,
+		$MaxHp = 0,
+		$Dps = 0
+	) {
+		$this->Upgrades = $Upgrades;
+		$this->DamagePerClick = $DamagePerClick;
+		$this->DamageMultiplierFire = $DamageMultiplierFire;
+		$this->DamageMultiplierWater = $DamageMultiplierWater;
+		$this->DamageMultiplierAir = $DamageMultiplierAir;
+		$this->DamageMultiplierEarth = $DamageMultiplierEarth;
+		$this->DamageMultiplierCrit = $DamageMultiplierCrit;
+		$this->UnlockedAbilitiesBitfield = $UnlockedAbilitiesBitfield;
+		$this->HpMultiplier = $HpMultiplier;
+		$this->CritPercentage = $CritPercentage;
+		$this->BadgePoints = $BadgePoints;
+		$this->AbilityItems = $AbilityItems;
+		$this->BossLootDropPercentage = $BossLootDropPercentage;
+		$this->DamageMultiplierDps = $DamageMultiplierDps;
+		$this->BaseDps = $BaseDps;
+		$this->DamagePerClickMultiplier = $DamagePerClickMultiplier;
+		$this->MaxHp = $MaxHp;
+		$this->Dps = $Dps;
+	}
+
+	public function ToArray()
+	{
+		return array(
+			"upgrades" => $this->GetUpgrades(),
+			"damage_per_click" => $this->GetDamagePerClick(),
+			"damage_multiplier_fire" => $this->GetDamageMultiplierFire(),
+			"damage_multiplier_water" => $this->GetDamageMultiplierWater(),
+			"damage_multiplier_air" => $this->GetDamageMultiplierAir(),
+			"damage_multiplier_earth" => $this->GetDamageMultiplierEarth(),
+			"damage_multiplier_crit" => $this->GetDamageMultiplierCrit(),
+			"unlocked_abilities_bitfield" => $this->GetUnlockedAbilitiesBitfield(),
+			"hp_multiplier" => $this->GetHpMultiplier(),
+			"crit_percentage" => $this->GetCritPercentage(),
+			"badge_points" => $this->GetBadgePoints(),
+			"ability_items" => $this->GetAbilityItems(),
+			"boss_loot_drop_percentage" => $this->GetBossLootDropPercentage(),
+			"damage_multiplier_dps" => $this->GetDamageMultiplierDps(),
+			"damage_per_click_multiplier" => $this->GetDamagePerClickMultiplier(),
+			"max_hp" => $this->GetMaxHp(),
+			"dps" => $this->GetDps()
+		);
+	}
+
+	public function GetUpgrades()
+	{
+		return $this->Upgrades;
+	}
+
+	public function GetDamagePerClick()
+	{
+		return $this->DamagePerClick;
+	}
+
+	public function GetDamageMultiplierFire()
+	{
+		return $this->DamageMultiplierFire;
+	}
+
+	public function GetDamageMultiplierWater()
+	{
+		return $this->DamageMultiplierWater;
+	}
+
+	public function GetDamageMultiplierAir()
+	{
+		return $this->DamageMultiplierAir;
+	}
+
+	public function GetDamageMultiplierEarth()
+	{
+		return $this->DamageMultiplierEarth;
+	}
+
+	public function GetDamageMultiplierCrit()
+	{
+		return $this->DamageMultiplierCrit;
+	}
+
+	public function GetUnlockedAbilitiesBitfield()
+	{
+		return $this->UnlockedAbilitiesBitfield;
+	}
+
+	public function GetHpMultiplier()
+	{
+		return $this->HpMultiplier;
+	}
+
+	public function GetCritPercentage()
+	{
+		return $this->CritPercentage;
+	}
+
+	public function GetBadgePoints()
+	{
+		return $this->BadgePoints;
+	}
+
+	public function GetAbilityItems()
+	{
+		return $this->AbilityItems;
+	}
+
+	public function GetBossLootDropPercentage()
+	{
+		return $this->BossLootDropPercentage;
+	}
+
+	public function GetDamageMultiplierDps()
+	{
+		return $this->DamageMultiplierDps;
+	}
+
+	public function GetBaseDps()
+	{
+		return $this->BaseDps;
+	}
+
+	public function GetDamagePerClickMultiplier()
+	{
+		return $this->DamagePerClickMultiplier;
+	}
+
+	public function GetMaxHp()
+	{
+		return $this->MaxHp;
+	}
+
+	public function GetDps()
+	{
+		return $this->Dps;
+	}
+}
+
+class CTowerAttackPlayerTechAbilityItem
+{
+	/*
+		optional ETowerAttackAbility ability = 1;
+		optional uint32 quantity = 2;
+	*/
+
+	private $Ability;
+	private $Quantity;
+	
+	public function __construct( $Ability, $Quantity )
+	{
+		$this->Ability = $Ability;
+		$this->Quantity = $Quantity;
+	}
+
+	public function GetAbility()
+	{
+		return $this->Ability;
+	}
+
+	public function GetQuantity()
+	{
+		return $this->Quantity;
+	}
+}
+
+class CTowerAttackPlayerTechTreeUpgrade
+{
+	/*
+	optional uint32 upgrade = 1;
+	optional uint32 level = 2;
+	optional double cost_for_next_level = 3;
+	*/
+	private $Upgrade;
+	private $Level;
+	private $CostForNextLevel;
+
+	public function __construct( $Upgrade, $Level, $CostForNextLevel )
+	{
+		$this->Upgrade = $Upgrade;
+		$this->Level = $Level;
+		$this->CostForNextLevel = $CostForNextLevel;
+	}
+
+	public function GetUpgrade()
+	{
+		return $this->Upgrade;
+	}
+
+	public function GetLevel()
+	{
+		return $this->Level;
+	}
+
+	public function GetCostForNextLevel()
+	{
+		return $this->CostForNextLevel;
 	}
 }
 
@@ -510,7 +773,27 @@ class CTowerAttackGame
 			0, // active abilities bitfield (generate from active abilities?)
 			array(), // active abilities
 			0, // crit damage
-			array() // loot
+			array(), // loot
+			new CTowerAttackPlayerTechTree( // tech tree
+				array(), // upgrades
+				1.0, // damage per click
+				$DamageMultiplierFire = 1.0, // damage multiplier fire
+				$DamageMultiplierWater = 1.0, // damage multiplier water
+				$DamageMultiplierAir = 1.0, // damage multiplier air
+				$DamageMultiplierEarth = 1.0, // damage multiplier earth
+				$DamageMultiplierCrit = 2.0, // damage multiplier crit
+				$UnlockedAbilitiesBitfield = 0, // unlocked abilities bitfield
+				$HpMultiplier = 1.0, // hp multiplier
+				$CritPercentage = 0, // crit percentage
+				$BadgePoints = 0, // badge points
+				$AbilityItems = array(), // ability items
+				$BossLootDropPercentage = 0.25, // boss loot drop percentage
+				$DamageMultiplierDps = 1.0, // damage multiplier dps
+				$BaseDps = 0, // base dps
+				$DamagePerClickMultiplier = 1.0, // damage per click multiplier
+				$MaxHp = 0, // max hp
+				$Dps = 0 // dps
+			)
 		);
 
 		$this->SetLevel( 0 );
