7d3fd5f194c17697ca38d690c6c99663a94957a2
Rossen Georgiev <zoorty@gmail.com>
2015-06-22 17:21:14 +0100

    fix waiting for players bar

--- javascript/minigame/towerattack/ui.js
+++ javascript/minigame/towerattack/ui.js
@@ -247,7 +247,7 @@ CUI.prototype.Tick = function()
 			{
 				numPlayers = game.m_rgStats.num_players;
 			}
-			var pct = (100* ( numPlayers / 1000 ) ).toFixed( 2 ) + '%' ;
+			var pct = (100* ( numPlayers / 15 ) ).toFixed( 2 ) + '%' ;
 			$J( "div.num_players_waiting_bar > div", this.m_dialogWaitingForPlayers )[0].style.width = pct;
 			$J( "#num_players_waiting", this.m_dialogWaitingForPlayers )[0].textContent = numPlayers ;
 			break;
