e1c73d5fc13fe133852a25f4efc863f8dfb0a7cd
Contex <contexmoh@gmail.com>
2015-06-29 19:05:16 +0200

    Only display INFO messages for now

--- php/src/TowerAttack/Logger.php
+++ php/src/TowerAttack/Logger.php
@@ -3,8 +3,12 @@ namespace SteamDB\CTowerAttack;
 
 class Logger extends \Psr\Log\AbstractLogger
 {
+
 	public function log( $level, $message, array $context = array() )
 	{
-		echo '[' . date( DATE_RSS ) . '] ' . '[' . $level . '] ' . $message . PHP_EOL;
+		if( $level === \Psr\Log\LogLevel::INFO ) # TODO: only display INFO messages for now
+		{
+			echo '[' . date( DATE_RSS ) . '] ' . '[' . $level . '] ' . $message . PHP_EOL;
+		}
 	}
 }
