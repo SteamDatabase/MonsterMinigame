25ae0eab4802e07f9880b6cb893f2dad8d836ce2
xPaw <github@xpaw.me>
2015-07-03 16:23:31 +0300

    Limit player name length

--- api/ITowerAttackMiniGameService/GetPlayerData/v0001/index.php
+++ api/ITowerAttackMiniGameService/GetPlayerData/v0001/index.php
@@ -12,7 +12,7 @@
 	Handle( INPUT_GET, [
 		'method' => 'GetPlayerData',
 		'steamid' => filter_input( INPUT_GET, 'steamid' ),
-		'player_name' => isset( $_SESSION[ 'Name' ] ) ? $_SESSION[ 'Name' ] : '[unnamed]',
+		'player_name' => isset( $_SESSION[ 'Name' ] ) ? mb_substr( $_SESSION[ 'Name' ], 0, 22, 'UTF-8' ) : '[unnamed]',
 		'include_tech_tree' => isset( $_GET[ 'include_tech_tree' ] ) && $_GET[ 'include_tech_tree' ],
 		'include_stats' => isset( $_GET[ 'include_stats' ] ) && $_GET[ 'include_stats' ]
 	] );
